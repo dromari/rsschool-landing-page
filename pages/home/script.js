37c55c1d6366994f62232c157866dfda46fd5453
Dromari <7709722@gmail.com>
2026-09-21 14:43:29 +0300

    feat: dark theme and fix style.css for home page

--- pages/home/script.js
+++ pages/home/script.js
@@ -213,6 +213,29 @@ const swipe = (elem) => {
 let elem = sliderRow;
 swipe(elem);
 
+document.addEventListener('DOMContentLoaded', () => {
+  const themeToggle = document.getElementById('theme-toggle');
+  const savedTheme = localStorage.getItem('theme');
+  
+  // Инициализация при загрузке
+  if (savedTheme === 'dark') {
+    document.body.classList.add('dark-theme');
+    themeToggle.checked = true;
+  }
+
+  // Переключение по клику
+  themeToggle.addEventListener('change', () => {
+    if (themeToggle.checked) {
+      document.body.classList.add('dark-theme');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.body.classList.remove('dark-theme');
+      localStorage.setItem('theme', 'light');
+    }
+  });
+});
+
+
 
 
 
